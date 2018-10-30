ca7cfa9dec0397fae42455348e2b67a9e91d8930
Arkadiusz Rogulski <arkadiusz.rogulski@gmail.com>
2018-10-30 23:01:49 +0100

    init commit

---
+++ src/main/java/com/eversis/spaceagencydatahub/SpaceAgencyDataHubApplication.java
@@ -0,0 +1,12 @@
+package com.eversis.spaceagencydatahub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceAgencyDataHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceAgencyDataHubApplication.class, args);
+	}
+}
---
+++ src/test/java/com/eversis/spaceagencydatahub/SpaceAgencyDataHubApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eversis.spaceagencydatahub;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpaceAgencyDataHubApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
